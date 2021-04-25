ddf3825b5d4126a5de1249c2ca1a8351d505047b
Paul <82410807+darknetworm@users.noreply.github.com>
2021-04-25 22:21:20 +0300

    Элементы списка, значения которых больше предыдущего элемента

---
+++ homeWork02.py
@@ -0,0 +1,17 @@
+firstList = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+finalList = []
+
+
+def readerList():
+    for item in firstList:
+        yield item
+
+
+firstNum = next(readerList())
+for num in readerList():
+    if firstNum < num:
+        finalList.append(num)
+    firstNum = num
+
+print('Исходный список: ' + str(firstList))
+print('Результирующий список: ' + str(finalList))
